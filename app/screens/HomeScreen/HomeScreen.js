6b26b13932afa267642b3b798f6e51340ccd495e
samuelmarina <samuellmarina@gmail.com>
2021-04-23 17:07:18 -0400

    Include title

--- app/screens/HomeScreen/HomeScreen.js
+++ app/screens/HomeScreen/HomeScreen.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Lottie from '../../components/Lottie/Lottie';
+import Title from '../../components/Title/Title';
+
+import colors from "../../config/colors/colors";
 
 function HomeScreen(props) {
     return (
@@ -9,6 +12,9 @@ function HomeScreen(props) {
                 source={require("../../assets/animations/Toaster/toaster.json")}
                 style={styles.toaster}
             />
+            <Title style={[styles.text, styles.shadow]}>
+                Sandwich Rater
+            </Title>
         </View>
     );
 }
@@ -19,6 +25,19 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center"
     },
+    shadow: {
+        textShadowColor: colors.shadow,
+        textShadowOffset: {
+            width: -1,
+            height: 2,
+        },
+        textShadowRadius: 10
+    },
+    text: {
+        fontSize: 60,
+        textAlign: "center",
+        color: colors.primary
+    },  
     toaster: {
         width: '100%'
     }
