807b91eea06095676559a6fe7eb29125fcb28907
samuelmarina <samuellmarina@gmail.com>
2021-04-24 00:13:39 -0400

    Initialize rating screen

---
+++ app/screens/RatingScreen/RatingScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+function RatingScreen(props) {
+    return (
+        <View style={styles.container}></View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
+
+export default RatingScreen;
