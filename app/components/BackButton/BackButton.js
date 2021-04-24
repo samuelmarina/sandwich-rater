13e0aca8cce913ff90c6df163cde2f43f7e64ec5
samuelmarina <samuellmarina@gmail.com>
2021-04-24 00:11:52 -0400

    Create back button component

---
+++ app/components/BackButton/BackButton.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+
+import { Ionicons } from '@expo/vector-icons';
+import colors from '../../config/colors/colors';
+
+function BackButton({
+    onPress,
+    size = 24,
+    color = colors.primary,
+    style
+}) {
+    return (
+        <TouchableOpacity 
+            style={[styles.container, style]}
+            onPress={onPress}
+        >
+            <Ionicons 
+                name='chevron-back'
+                size={size}
+                color={color}
+            />
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
+
+export default BackButton;
